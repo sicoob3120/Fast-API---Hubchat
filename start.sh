e34924115423cef3cba2d6d5dd4622d83fe68481
Matheus Tomaz Souza <51293473+mathtomaz26@users.noreply.github.com>
2025-06-05 15:53:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "Iniciando servidor Uvicorn na porta 8080..."
-exec uvicorn main:app --host 0.0.0.0 --port=8080 --lifespan on --timeout-keep-alive 120 --log-level debug
+uvicorn main:app --host 0.0.0.0 --port 8000
