e36954bf2783840c1d39ea2043463ef37230b834
Sicoob Frutal <67108125+sicoob3120@users.noreply.github.com>
2025-07-08 15:21:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_html
 from routes import router
 import os
 
@@ -24,10 +25,20 @@ def custom_openapi():
             "name": "x-api-key"
         }
     }
+    # Aplica segurança em todas as rotas, exceto as do Swagger
     for path in openapi_schema["paths"]:
-        for method in openapi_schema["paths"][path]:
-            openapi_schema["paths"][path][method]["security"] = [{"ApiKeyAuth": []}]
+        if not path.startswith("/docs") and not path.startswith("/openapi") and not path.startswith("/redoc"):
+            for method in openapi_schema["paths"][path]:
+                openapi_schema["paths"][path][method]["security"] = [{"ApiKeyAuth": []}]
     app.openapi_schema = openapi_schema
     return app.openapi_schema
 
 app.openapi = custom_openapi
+
+# Rota manual para visualização do Swagger
+@app.get("/documentacao", include_in_schema=False)
+async def custom_swagger_ui():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="Documentação Reccredito"
+    )
